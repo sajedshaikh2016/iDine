30ebde7fc78de89a1c3c0791afc36391d6626dcf
Sajed Shaikh <sajed.shaikh@hotmail.com>
2022-11-19 17:21:45 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Sajed Shaikh on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
