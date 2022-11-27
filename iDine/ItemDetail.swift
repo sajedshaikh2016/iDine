044e859c39e347f6a349eb2cd5b26f83a2d82f34
Sajed Shaikh <sajed.shaikh@hotmail.com>
2022-11-27 13:49:11 +0530

    Adding items to an order with @EnvironmentObject

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ItemDetail: View {
+    @EnvironmentObject var order: Order
     let item: MenuItem
     var body: some View {
         VStack {
@@ -25,6 +26,12 @@ struct ItemDetail: View {
             }
             Text(item.description)
                 .padding()
+            
+            Button("Order This") {
+                order.add(item: item)
+            }
+            .font(.headline)
+            
             Spacer()
         }
         .navigationTitle(item.name)
@@ -36,6 +43,7 @@ struct ItemDetail_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             ItemDetail(item: MenuItem.example)
+                .environmentObject(Order())
         }
     }
 }
---
+++ iDine/OrderView.swift
@@ -0,0 +1,45 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Sajed Shaikh on 27/11/22.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: Text("Check Out")) {
+                        Text("Place Order")
+                    }
+                }
+                
+            }
+            .navigationTitle("Order")
+            .listStyle(InsetGroupedListStyle())
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+            .environmentObject(Order())
+    }
+}
