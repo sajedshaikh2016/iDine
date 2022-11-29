f2c6f16569bdabbbad93e2128dfdae8b6f1582eb
Sajed Shaikh <sajed.shaikh@hotmail.com>
2022-11-29 17:10:13 +0530

    Bindings and forms

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,36 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Sajed Shaikh on 29/11/22.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    @State private var paymentType = "Cash"
+    
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView()
+            .environmentObject(Order())
+    }
+}
